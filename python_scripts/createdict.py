878306d6253cd0a5aa6463537cd0329934d79a29
BiswasDebjyoti <debjyoti.biswas.che14@iitbhu.ac.in>
2017-10-20 12:04:29 +0530

    changes made

--- python_scripts/createdict.py
+++
@@ -1,8 +0,0 @@
-#dict_pop={'':,} 
-import csv
-import pprint
-reader = csv.DictReader(open('/home/debjyoti/Desktop/Manila-Tredence-Beverage/xls_files/populationden.csv', 'rb'))
-dict_list = []
-for line in reader:
-    dict_list.append(line)
-pprint.pprint(dict_list)
