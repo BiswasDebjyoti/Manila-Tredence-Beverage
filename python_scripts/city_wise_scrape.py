b2039e5b638e22e0f0a77e93e7989c968f49d595
BiswasDebjyoti <debjyoti.biswas.che14@iitbhu.ac.in>
2017-10-25 21:12:38 +0530

    suck it up!

--- python_scripts/city_wise_scrape.py
+++ python_scripts/city_wise_scrape.py
@@ -7,52 +7,52 @@ count = 0
 #create a spreadsheet file
 wb=xlwt.Workbook()
 #create a sheet
-munci=['Manila','Makati','Mandaluyong','San Juan','Pasig','Pasay','Quezon','Quezon','Taguig','Paranaque','North Caloocan','South Caloocan','Las Pinas','Malabon','Marikina','Navotas','Valenzuela','Muntinlupa']
-for i in munci:
-    ws = wb.add_sheet(i+' City')
-    url="https://en.yelp.com.ph/search?find_desc=Grocery&find_loc="+i+",+Metro+Manila&start="
-    for x in range(0,600,10):
-        _url=url+str(x)
-        r = requests.get(_url)
+
+
+ws = wb.add_sheet(i+' City')
+url="https://en.yelp.com.ph/search?find_desc=Grocery&find_loc=Makati,+Metro+Manila&start="
+for x in range(0,600,10):
+    _url=url+str(x)
+    r = requests.get(_url)
         #create a BS instance using the request data
-        soup = BeautifulSoup(r.content,"lxml")
+    soup = BeautifulSoup(r.content,"lxml")
         
-        g_data = soup.find_all("div",{"class":"search-result natural-search-result"})
-        for item in g_data:
-            name=item.contents[1].contents[1].contents[1].contents[3].contents[1].contents[1].contents[1].text
-            addr =None
+    g_data = soup.find_all("div",{"class":"search-result natural-search-result"})
+    for item in g_data:
+        name=item.contents[1].contents[1].contents[1].contents[3].contents[1].contents[1].contents[1].text
+        addr =None
             
-            try:
-                phn=str(item.contents[1].contents[3].contents[7].text)
-                phn=str(phn.replace('\n            ','',1))
-                phn=phn.strip("\n    ")
-            except:
-                continue
-            try:
-                addr=item.contents[1].contents[3].contents[3].text
-                addr=str(addr.replace('\n            ','',1))
-                addr=addr.strip("\n    ")
-            except:
-                continue
-            try:
-                city=str(item.contents[1].contents[3].contents[1].text)
-                city=str(city.replace('\n        ','',1))
-                city=city.strip("        ")
-            except:
-                continue
+        try:
+            phn=str(item.contents[1].contents[3].contents[7].text)
+            phn=str(phn.replace('\n            ','',1))
+            phn=phn.strip("\n    ")
+        except:
+            continue
+        try:
+            addr=item.contents[1].contents[3].contents[3].text
+            addr=str(addr.replace('\n            ','',1))
+            addr=addr.strip("\n    ")
+        except:
+            continue
+        try:
+            city=str(item.contents[1].contents[3].contents[1].text)
+            city=str(city.replace('\n        ','',1))
+            city=city.strip("        ")
+        except:
+            continue
             #print addr
             
-            if city == i +' City':
-                print i
-                print name
-                if addr!=None:
+        if city == 'Makati City':
+            #print i
+            print name
+            if addr=='Makati City':
                 
-                    count = count + 1
-                    ws=wb.get_sheet(i+' City')
-                    ws.write(count-1,0,name)
-                    ws.write(count-1,1,city)
-                    ws.write(count-1,2,addr)
-                    ws.write(count-1,3,phn)
+                count = count + 1
+                #ws=wb.get_sheet(i+' City')
+                ws.write(count-1,0,name)
+                ws.write(count-1,1,city)
+                ws.write(count-1,2,addr)
+                ws.write(count-1,3,phn)
         
         wb.save("/home/debjyoti/Desktop/Manila-Tredence-Beverage/xls_files/BeveragesYelpData.xls")   
 
